fe4ea91484685876e01a372a9837acdecd8146af
Philmon-Stefanos <Philmon.Stefanos001@mymdc.net>
2021-10-02 00:58:37 +0000

    ExcuseGenerator step 3

---
+++ app.js
@@ -0,0 +1,14 @@
+
+function excuseGenerator(){
+    debugger
+    let who = ['The dog','My grandma','His turtle','My bird'];
+    let action = ['ate','peed','crushed','broke'];
+    let what = ['my homework', 'the keys', 'the car'];
+    let when = ['before the class','right on time','when I finished','during my lunch','while I was praying'];
+    console.log(who[0] + ' ' + action[2] + ' ' + what[0] + ' ' + when[0])
+}
+excuseGenerator()
+const search = document.querySelector("p")
+const replace = element.innerHTML("search");
+
+element.innerHTML = htmlString;
