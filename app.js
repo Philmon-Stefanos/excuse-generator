70bd3c3aa258499f308eb3c1fe017fbcc7a5b59d
Philmon-Stefanos <Philmon.Stefanos001@mymdc.net>
2021-10-06 22:40:20 +0000

    onLoad added

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
-
 function excuseGenerator(){
     let who = ['The dog','My grandma','His turtle','My bird'];
     let action = ['ate','peed on','crushed','broke'];
     let what = ['my homework', 'the keys', 'the car'];
     let when = ['before the class','right on time','when I finished','during my lunch','while I was praying'];
     console.log(who[Math.floor(Math.random()*3+1)] + ' ' + action[Math.floor(Math.random()*3+1)] + ' ' + what[Math.floor(Math.random()*2+1)] + ' ' + when[Math.floor(Math.random()*4+1)]);
-    return who[Math.floor(Math.random()*3+1)] + ' ' + action[Math.floor(Math.random()*3+1)] + ' ' + what[Math.floor(Math.random()*2+1)] + ' ' + when[Math.floor(Math.random()*4+1)];
+    return who[Math.floor(Math.random()*3)] + ' ' + action[Math.floor(Math.random()*3)] + ' ' + what[Math.floor(Math.random()*2)] + ' ' + when[Math.floor(Math.random()*4)];
 };
 let search = document.querySelector("p");
 search.innerHTML = excuseGenerator();
